705d595ccce8c8c0a7eefbf086fd5d1e3dfb57ba
rfduarte <ramon7.freitas@gmail.com>
2018-04-26 10:32:30 -0300

    Add files via upload

---
+++ Check-Services.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Is this script running in pre or post boot?"
+    $PreOrPosBoot = Read-Host "( Pre / Pos )"
+    Switch ($PreOrPosBoot)
+    {
+        Pre {Get-Service | Select Status,Name,StartType | Export-Csv C:\Users\ramon.duarte\Desktop\PreBootServices.csv}
+        Pre {Write-Host "Done!"}
+        Pos {Get-Service | Select Status,Name,StartType | Export-Csv C:\Users\ramon.duarte\Desktop\PosBootServices.csv}
+        Pos {Compare-Object (Get-Content C:\Users\ramon.duarte\Desktop\PreBootServices.csv) (Get-Content C:\Users\ramon.duarte\Desktop\PosBootServices.csv)}
+    }
